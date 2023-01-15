b8aa3c6624a0dd84529fe8c069fb6bcfadfbe989
github_Devloper-Belal <asaamman123.bk@gmail.com>
2023-01-15 03:13:44 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+<>
+  <h1 className='app'>devleoper-belal</h1>
+  <h2 className='app'>balasmeh</h2>
+  <h1 className='app'>devleoper-belal</h1>
+  <h2 className='app'>balasmeh</h2>
+  <h1 className='app'>devleoper-belal</h1>
+  <h2 className='app'>balasmeh</h2>
+</>
+  );
+}
+
+export default App;
